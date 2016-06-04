635c067e26b45999750cd900966dc85704c4ad6d
Max Ivak <maxivak@gmail.com>
2016-06-04 16:03:52 +0300

    respect OptimaCMS namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,13 @@
 OptimacmsOptions::Engine.routes.draw do
 #Rails.application.routes.draw do
-  get '/mmm/options', to: 'admin/options#index'
+  #get '/mmm/options', to: 'admin/options#index'
 
-  scope '/cmsadmin' do
-    #, module: "admin", as: 'admin' do
-    scope module: 'admin', as: 'admin' do
+  scope '/'+Optimacms.admin_namespace, module: "admin" do
       # options
       resources :options do
         collection do
           post 'search'
         end
       end
-    end
   end
 end
