b58a49a7b84ff41a457261776129d3db02d80a61
Max Ivak <maxivak@gmail.com>
2016-06-04 16:54:40 +0300

    Untrack ignored files!

---
+++ spec/dummy/db/migrate/20160604135417_create_options.rb
@@ -0,0 +1,16 @@
+class CreateOptions < ActiveRecord::Migration
+  def change
+    create_table :options do |t|
+      t.string :name, :null => false
+      t.string :title, :null => false
+      t.string :option_type, :null => true
+      t.text :description, :null => true
+      t.boolean :is_changed, :null => false, :default=>1
+      t.string :category, :null => true
+      t.string :value, :null => true
+
+    end
+
+    add_index :options, :name, :unique => true
+  end
+end
